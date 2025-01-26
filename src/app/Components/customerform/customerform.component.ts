f1d36e70c53bcfb8bac5573c199b6b84717a5736
Ajay Saravanan <avsoftwaredevelopers@gmail.com>
2025-01-26 13:53:40 +0530

    Initialized Components and Angular Materials

---
+++ src/app/Components/customerform/customerform.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customerform',
+  imports: [],
+  templateUrl: './customerform.component.html',
+  styleUrl: './customerform.component.css'
+})
+export class CustomerformComponent {
+
+}
